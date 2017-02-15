bdfa098cfc5279e7f4c83681ca6c8b85c96defdd
Qiong524 <rising4250@gmail.com>
2017-02-15 23:08:46 +0800

    AI

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ var over = false;
 
 //贏法數組
 var wins = [];
-var count = 0;
 
 //贏法的統計數組
 var myWin = []; //我方
@@ -25,6 +24,7 @@ for (var i = 0; i < 15; i++) {
 	}
 }
 
+var count = 0;
 // //所有直線的贏法
 for (var i = 0; i < 15; i++) {
 	for (var j = 0; j < 11; j++) {
@@ -170,18 +170,16 @@ chess.onclick = function(e) {
 	if(over) {
 		return;
 	}
+	if(!me) {
+		return;
+	}
 	var x = e.offsetX;
 	var y = e.offsetY;
 	var i = Math.floor(x / 30); //向下取整數
 	var j = Math.floor(y / 30);
 	if (chessBoard[i][j] == 0) {
 		oneStep(i, j, me);
-		if(me) {
-			chessBoard[i][j] = 1;
-		} else {
-			chessBoard[i][j] = 2;
-		}
-		me = !me;
+		chessBoard[i][j] = 1;
 		for (var k = 0; k < count; k++) {
 			if(wins[i][j][k]) {
 				myWin[k]++;
@@ -192,6 +190,93 @@ chess.onclick = function(e) {
 				}
 			}
 		}
+		if(!over) {
+			me = !me;
+			computerAI();
+		}
+	}
+}
+
+var computerAI = function() {
+	var myScore = [];
+	var computerScore = [];
+	var max = 0; //保存最高分
+	var u = 0, v = 0; //保存最高分數點的座標
+	for (var i = 0; i < 15; i++) {
+		myScore[i] = [];
+		computerScore[i] = [];
+		for (var j = 0; j < 15; j++) {
+			myScore[i][j] = 0;
+			computerScore[i][j] = 0;
+		}
+	}
+	for (var i = 0; i < 15; i++) {
+		for (var j = 0; j < 15; j++) {
+			if(chessBoard[i][j] == 0) {
+				for (var k = 0; k < count; k++) {
+					if(wins[i][j][k]){
+
+						if(myWin[k] == 1) {
+							myScore[i][j] += 200;
+						} else if(myWin[k] == 2) {
+							myScore[i][j] += 400;
+						} else if(myWin[k] == 3) {
+							myScore[i][j] += 2000;
+						} else if(myWin[k] == 4) {
+							myScore[i][j] += 10000;
+						} 	
+
+						if(computerWin[k] == 1) {
+							computerScore[i][j] += 220;
+						} else if(computerWin[k] == 2) {
+							computerScore[i][j] += 420;
+						} else if(computerWin[k] == 3) {
+							computerScore[i][j] += 2100;
+						} else if(computerWin[k] == 4) {
+							computerScore[i][j] += 20000;
+						} 	
+
+					}	
+				}
+
+				if(myScore[i][j] > max) {
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				} else if(myScore[i][j] == max) {
+					if(computerScore[i][j] > computerScore[u][v]) {
+						u = i;
+						v = j;
+					}
+				}
+
+				if(computerScore[i][j] > max) {
+					max = computerScore[i][j];
+					u = i;
+					v = j;
+				} else if(computerScore[i][j] == max) {
+					if(myScore[i][j] > myScore[u][v]) {
+						u = i;
+						v = j;
+					}
+				}
+			}
+		}
+	}
+	oneStep(u, v, false);
+	chessBoard[u][v] = 2;
+	for (var k = 0; k < count; k++) {
+		if(wins[u][v][k]) {
+			computerWin[k]++;
+			myWin[k] = 6;
+			if(computerWin[k] == 5) {
+				window.alert("AI贏了");
+				over = true;
+			}
+		}
+	}
+	if(!over) {
+		me = !me;
 	}
 }
 
